8856407d1c01b397afde28ce812cca3aac7a1f80
felipenasci <dfnascimento@live.com>
2024-05-26 11:05:58 -0300

    chore: config contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+import Config from "../config/index.js";
+
+export async function getStaticProps() {
+  const client = createClient(Config.contentful);
+  const { items: recipes } = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  return { props: { recipes } };
+}
+
+export default function Recipes({ recipes }) {
+  return <div className="recipe-list"> Recipe List</div>;
 }
