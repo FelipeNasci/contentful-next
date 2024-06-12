b3d6bedfb5d42cd9884e47c0df8e759f1d82f58d
felipenasci <dfnascimento@live.com>
2024-06-11 21:29:09 -0300

    refactor(Layout): add memorization to get more performance

---
+++ components/Layout/components/Footer.js
@@ -0,0 +1,11 @@
+import { memo } from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2021 Just Add Marmite :)</p>
+    </footer>
+  );
+}
+
+export default memo(Footer);
